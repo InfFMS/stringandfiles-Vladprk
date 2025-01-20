a77d13ffb16f81673feb83b6ca912ad010f7f3b3
Vlados <vladprkrf@mail.ru>
2025-01-20 09:00:12 +0700

    :)

--- task5.py
+++ task5.py
@@ -3,5 +3,25 @@
 # Запишите это слово и его длину в новый файл в формате:
 # Самое длинное слово: слово
 # Его длина: длина
-#
 # Выведите это слово и длину в консоль.
+f = open('task5.txt', 'r', encoding='utf-8')
+new_f = open('new_file_for_task_5.txt', 'w', encoding='utf-8')
+a = f.read()
+the_longest_word = ''
+length_of_the_longest_word = -1
+length = 0
+word = ''
+for i in range(len(a)):
+    indi = False
+    if a[i] != " " and a[i] != "\n":
+        if a[i] != '.' and a[i] != ',':
+            length += 1
+            word += a[i]
+    else:
+        if length_of_the_longest_word < length:
+            length_of_the_longest_word = length
+            the_longest_word = word
+        word = ''
+        length = 0
+new_f.write(f'Самое длинное слово: "{the_longest_word}" \n')
+new_f.write(f'Его длина: {length_of_the_longest_word}')
